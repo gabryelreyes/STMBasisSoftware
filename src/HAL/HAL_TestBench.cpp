16f540491c5f625724831f73e0efb0f9530e3466
greyes <gabryelrdiaz@gmail.com>
2022-10-24 10:22:35 +0200

    Refactoring of GPIO initialization

--- src/HAL/HAL_TestBench.cpp
+++ src/HAL/HAL_TestBench.cpp
@@ -51,9 +51,15 @@ SOFTWARE.
 bool SystemClock_Config(void);
 
 /** 
- *  Configure GPIO Pins
+ *  Configure GPIO Ports.
+ *  Sets all Pins to Analog Input.
  */
-void MX_GPIO_Init(void);
+void initGpioPorts(void);
+
+/**
+ *  Configure the GPIO Pins for the current Board.
+ */
+void initBoardGpio(void);
 
 /******************************************************************************
  * Local Variables
@@ -80,8 +86,12 @@ bool HAL_TestBench::init()
         isSuccess = false;
     }
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
+    // Initialize GPIO Banks. Sets all Pins to Analog Input.
+    initGpioPorts();
+
+    // Initialize GPIO for the current Board.
+    initBoardGpio();
+
     return isSuccess;
 }
 
@@ -219,95 +229,207 @@ bool SystemClock_Config(void)
     return isSuccess;
 }
 
-void MX_GPIO_Init(void)
+void initGpioPorts ()
 {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+    // Configure Bank A
+    // Do NOT reconfigure Pins 13, 14 and 15. These belong to JTAG.
+    GPIO_InitStruct.Pin =   GPIO_PIN_0|
+                            GPIO_PIN_1|
+                            GPIO_PIN_2|
+                            GPIO_PIN_3|
+                            GPIO_PIN_4|
+                            GPIO_PIN_5|
+                            GPIO_PIN_6|
+                            GPIO_PIN_7|
+                            GPIO_PIN_8|
+                            GPIO_PIN_9|
+                            GPIO_PIN_10|
+                            GPIO_PIN_11|
+                            GPIO_PIN_12;
+                            // GPIO_PIN_13|
+                            // GPIO_PIN_14|
+                            // GPIO_PIN_15;
+    
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOE_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOE, LED_Blue_Pin|LED_Green_Pin, GPIO_PIN_RESET);
+    // Configure Bank B
+    // Do NOT reconfigure Pin 3. It belongs to JTAG.
+    GPIO_InitStruct.Pin =   GPIO_PIN_0|
+                            GPIO_PIN_1|
+                            GPIO_PIN_2|
+                            // GPIO_PIN_3|
+                            GPIO_PIN_4|
+                            GPIO_PIN_5|
+                            GPIO_PIN_6|
+                            GPIO_PIN_7|
+                            GPIO_PIN_8|
+                            GPIO_PIN_9|
+                            GPIO_PIN_10|
+                            GPIO_PIN_11|
+                            GPIO_PIN_12|
+                            GPIO_PIN_13|
+                            GPIO_PIN_14|
+                            GPIO_PIN_15;
+    
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PEPin PEPin */
-  GPIO_InitStruct.Pin = LED_Blue_Pin|LED_Green_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+    // Configure Bank C
+    GPIO_InitStruct.Pin =   GPIO_PIN_0|
+                            GPIO_PIN_1|
+                            GPIO_PIN_2|
+                            GPIO_PIN_3|
+                            GPIO_PIN_4|
+                            GPIO_PIN_5|
+                            GPIO_PIN_6|
+                            GPIO_PIN_7|
+                            GPIO_PIN_8|
+                            GPIO_PIN_9|
+                            GPIO_PIN_10|
+                            GPIO_PIN_11|
+                            GPIO_PIN_12|
+                            GPIO_PIN_13|
+                            GPIO_PIN_14|
+                            GPIO_PIN_15;
 
-  /*Configure GPIO pins : PE3 PE4 PE5 PE6
-                           PE7 PE8 PE9 PE10
-                           PE11 PE12 PE13 PE14
-                           PE15 PE0 */
-  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
-                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10
-                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
-                          |GPIO_PIN_15|GPIO_PIN_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PC13 PC14 PC15 PC0
-                           PC1 PC2 PC3 PC4
-                           PC5 PC6 PC7 PC8
-                           PC9 PC10 PC11 PC12 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_0
-                          |GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
-                          |GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
-                          |GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    // Configure Bank D
+    GPIO_InitStruct.Pin =   GPIO_PIN_0|
+                            GPIO_PIN_1|
+                            GPIO_PIN_2|
+                            GPIO_PIN_3|
+                            GPIO_PIN_4|
+                            GPIO_PIN_5|
+                            GPIO_PIN_6|
+                            GPIO_PIN_7|
+                            GPIO_PIN_8|
+                            GPIO_PIN_9|
+                            GPIO_PIN_10|
+                            GPIO_PIN_11|
+                            GPIO_PIN_12|
+                            GPIO_PIN_13|
+                            GPIO_PIN_14|
+                            GPIO_PIN_15;
 
-  /*Configure GPIO pins : PH0 PH1 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA0 PA1 PA2 PA3
-                           PA4 PA5 PA6 PA7
-                           PA8 PA9 PA10 PA11
-                           PA12 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
-                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
-                          |GPIO_PIN_12;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    // Configure Bank E
+    GPIO_InitStruct.Pin =   GPIO_PIN_0|
+                            GPIO_PIN_1|
+                            GPIO_PIN_2|
+                            GPIO_PIN_3|
+                            GPIO_PIN_4|
+                            GPIO_PIN_5|
+                            GPIO_PIN_6|
+                            GPIO_PIN_7|
+                            GPIO_PIN_8|
+                            GPIO_PIN_9|
+                            GPIO_PIN_10|
+                            GPIO_PIN_11|
+                            GPIO_PIN_12|
+                            GPIO_PIN_13|
+                            GPIO_PIN_14|
+                            GPIO_PIN_15;
 
-  /*Configure GPIO pins : PB0 PB1 PB2 PB10
-                           PB11 PB12 PB13 PB14
-                           PB15 PB4 PB5 PB6
-                           PB7 PB8 PB9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10
-                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
-                          |GPIO_PIN_15|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
-                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PD8 PD9 PD10 PD11
-                           PD12 PD13 PD14 PD15
-                           PD0 PD1 PD2 PD3
-                           PD4 PD5 PD6 PD7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
-                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15
-                          |GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+    // Configure Bank H
+    // Port H only has Pins 0 and 1
+    GPIO_InitStruct.Pin =   GPIO_PIN_0|
+                            GPIO_PIN_1;
+                            // GPIO_PIN_2|
+                            // GPIO_PIN_3|
+                            // GPIO_PIN_4|
+                            // GPIO_PIN_5|
+                            // GPIO_PIN_6|
+                            // GPIO_PIN_7|
+                            // GPIO_PIN_8|
+                            // GPIO_PIN_9|
+                            // GPIO_PIN_10|
+                            // GPIO_PIN_11|
+                            // GPIO_PIN_12|
+                            // GPIO_PIN_13|
+                            // GPIO_PIN_14|
+                            // GPIO_PIN_15;
 
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+}
+
+void initBoardGpio(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /*Configure BMS_OK */
+    GPIO_InitStruct.Pin = BMS_OK_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_DeInit(BMS_OK_GPIO_Port, BMS_OK_Pin);
+    HAL_GPIO_WritePin(BMS_OK_GPIO_Port, BMS_OK_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_Init(BMS_OK_GPIO_Port, &GPIO_InitStruct);
+
+
+    /*Configure SPI_CS_1 */
+    GPIO_InitStruct.Pin = NSS_1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+    HAL_GPIO_DeInit(NSS_1_GPIO_Port, NSS_1_Pin);
+    HAL_GPIO_WritePin(NSS_1_GPIO_Port, NSS_1_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_Init(NSS_1_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure SPI_CS_2 */
+    GPIO_InitStruct.Pin = NSS_2_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+    HAL_GPIO_DeInit(NSS_2_GPIO_Port, NSS_2_Pin);
+    HAL_GPIO_WritePin(NSS_2_GPIO_Port, NSS_2_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_Init(NSS_2_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure Red LED */
+    GPIO_InitStruct.Pin = LED_Red_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_DeInit(LED_Red_GPIO_Port, LED_Red_Pin);
+    HAL_GPIO_WritePin(LED_Red_GPIO_Port, LED_Red_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_Init(LED_Red_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure Blue LED */
+    GPIO_InitStruct.Pin = LED_Blue_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_DeInit(LED_Blue_GPIO_Port, LED_Blue_Pin);
+    HAL_GPIO_WritePin(LED_Blue_GPIO_Port, LED_Blue_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_Init(LED_Blue_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure Green LED */
+    GPIO_InitStruct.Pin = LED_Green_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_DeInit(LED_Green_GPIO_Port, LED_Green_Pin);
+    HAL_GPIO_WritePin(LED_Green_GPIO_Port, LED_Green_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_Init(LED_Green_GPIO_Port, &GPIO_InitStruct);
 }
 
 /******************************************************************************
