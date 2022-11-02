85f1ca0e8005ed1b4c34f62bec7f37a0245b8ab4
greyes <gabryelrdiaz@gmail.com>
2022-11-02 15:58:13 +0100

    Fixed Formatting and documentation

--- src/HAL/GPIO.h
+++ src/HAL/GPIO.h
@@ -46,89 +46,84 @@ SOFTWARE.
 /******************************************************************************
  * Types and Classes
  *****************************************************************************/
-
+/**
+ *  Namespace for User GPIO.
+ */
 namespace GPIO
 {
 
 #ifdef Discovery_DevBoard
-/** SPI 1 CS */
-static DigitalOut NSS_1(GPIOA, 
-                        GPIO_PIN_4,
-                        GPIO_NOPULL, 
-                        GPIO_SPEED_FREQ_VERY_HIGH);
+    /** SPI 1 CS */
+    static DigitalOut NSS_1(GPIOA,
+                            GPIO_PIN_4,
+                            GPIO_NOPULL,
+                            GPIO_SPEED_FREQ_VERY_HIGH);
 
-/** SPI 2 CS */
-static DigitalOut NSS_2(GPIOB, 
-                        GPIO_PIN_9,
-                        GPIO_NOPULL, 
-                        GPIO_SPEED_FREQ_VERY_HIGH);
+    /** SPI 2 CS */
+    static DigitalOut NSS_2(GPIOB,
+                            GPIO_PIN_9,
+                            GPIO_NOPULL,
+                            GPIO_SPEED_FREQ_VERY_HIGH);
 
-static DigitalOut LED_BLUE(GPIOD, GPIO_PIN_15);
+    static DigitalOut LED_BLUE(GPIOD, GPIO_PIN_15);
 
-static DigitalOut LED_RED(GPIOD, GPIO_PIN_14);
+    static DigitalOut LED_RED(GPIOD, GPIO_PIN_14);
 
-static DigitalOut LED_ORANGE(GPIOD, GPIO_PIN_13);
+    static DigitalOut LED_ORANGE(GPIOD, GPIO_PIN_13);
 
-static DigitalOut LED_GREEN(GPIOD, GPIO_PIN_12);
+    static DigitalOut LED_GREEN(GPIOD, GPIO_PIN_12);
 
-static DigitalIn userButton(GPIOA, GPIO_PIN_0);
+    static DigitalIn userButton(GPIOA, GPIO_PIN_0);
 
-/** A list of all used i/o pins, used for initialization. */
-static const PIN* pinList[] =
-{
-    &NSS_1,
-    &NSS_2,
-    &LED_RED,
-    &LED_GREEN,
-    &LED_BLUE,
-    &LED_ORANGE,
-    &userButton
-};
+    /** A list of all used i/o pins, used for initialization. */
+    static const PIN *pinList[] =
+    {
+        &NSS_1,
+        &NSS_2,
+        &LED_RED,
+        &LED_GREEN,
+        &LED_BLUE,
+        &LED_ORANGE,
+        &userButton
+    };
 
 #endif /* Discovery_DevBoard */
 
 #ifdef BMS
-/** SPI 1 CS */
-static DigitalOut NSS_1(GPIOA, 
-                        GPIO_PIN_4,
-                        GPIO_NOPULL, 
-                        GPIO_SPEED_FREQ_VERY_HIGH);
+    /** SPI 1 CS */
+    static DigitalOut NSS_1(GPIOA,
+                            GPIO_PIN_4,
+                            GPIO_NOPULL,
+                            GPIO_SPEED_FREQ_VERY_HIGH);
 
-/** SPI 2 CS */
-static DigitalOut NSS_2(GPIOB, 
-                        GPIO_PIN_9,
-                        GPIO_NOPULL, 
-                        GPIO_SPEED_FREQ_VERY_HIGH);
+    /** SPI 2 CS */
+    static DigitalOut NSS_2(GPIOB,
+                            GPIO_PIN_9,
+                            GPIO_NOPULL,
+                            GPIO_SPEED_FREQ_VERY_HIGH);
 
-/** Red LED */
-// static DigitalOut LED_RED(GPIOE, GPIO_PIN_0); // BMS PCB
-static DigitalOut LED_RED(GPIOD, GPIO_PIN_15); // DISCO DevBoard
+    /** Red LED */
+    static DigitalOut LED_RED(GPIOE, GPIO_PIN_0);
 
-/** Green LED */
-// static DigitalOut LED_GREEN(GPIOE, GPIO_PIN_1); // BMS PCB
-static DigitalOut LED_GREEN(GPIOD, GPIO_PIN_14); // DISCO DevBoard
+    /** Green LED */
+    static DigitalOut LED_GREEN(GPIOE, GPIO_PIN_1);
 
-/** Blue LED */
-// static DigitalOut LED_BLUE(GPIOE, GPIO_PIN_2); // BMS PCB
-static DigitalOut LED_BLUE(GPIOD, GPIO_PIN_13); // DISCO DevBoard
+    /** Blue LED */
+    static DigitalOut LED_BLUE(GPIOE, GPIO_PIN_2);
 
-/** BMS OK */
-// static DigitalOut BMS_OK(GPIOE, GPIO_PIN_15); // BMS PCB
-static DigitalOut BMS_OK(GPIOD, GPIO_PIN_12); // DISCO DevBoard
+    /** BMS OK */
+    static DigitalOut BMS_OK(GPIOE, GPIO_PIN_15);
 
-static DigitalIn TestButton(GPIOA, GPIO_PIN_0);
-
-/** A list of all used i/o pins, used for initialization. */
-static const PIN* pinList[] =
-{
-    &NSS_1,
-    &NSS_2,
-    &LED_RED,
-    &LED_GREEN,
-    &LED_BLUE,
-    &BMS_OK,
-    &TestButton
-};
+    /** A list of all used i/o pins, used for initialization. */
+    static const PIN *pinList[] =
+    {
+        &NSS_1,
+        &NSS_2,
+        &LED_RED,
+        &LED_GREEN,
+        &LED_BLUE,
+        &BMS_OK
+    };
 
 #endif /* BMS */
 
