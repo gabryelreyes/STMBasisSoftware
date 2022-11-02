8ca1ae53cdbb215a4734fa2f616efcda0b353a05
greyes <gabryelrdiaz@gmail.com>
2022-11-02 07:46:27 +0100

    Added utilities

---
+++ lib/Common/util.h
@@ -0,0 +1,54 @@
+/* MIT License
+
+Copyright (c) 2022 Gabryel Reyes <gabryelrdiaz@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ *  @brief  Utilities
+ *  @author Gabryel Reyes <gabryelrdiaz@gmail.com>
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/** Get number of array elements. */
+#define UTIL_ARRAY_NUM(__arr)   (sizeof(__arr) / sizeof((__arr)[0]))
+
+/** Use it to mark not used function parameters. */
+#define UTIL_NOT_USED(__var)    (void)(__var)
+
+/******************************************************************************
+ * Types and Classes
+ *****************************************************************************/
+
+#endif /* UTIL_H_ */
